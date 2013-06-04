50502211a3afe1f2743fe5fdf45e2dd6c3f65b5e
olivierb <olivierb@9ce8b743-0909-46e4-8230-39c73aa7ff53>
2013-06-04 10:04:25 +0000

    * Packaging.
    git-svn-id: http://open.iabsis.com/subversion/igestis-tcpdf/trunk@1826 9ce8b743-0909-46e4-8230-39c73aa7ff53

---
+++ Tcpdf.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Igestis\Modules\Tcpdf;
+
+include __DIR__ . "/Tcpdf/tcpdf.php";
+
+/**
+ * Container for the tcpdf original class. Implements some helper.
+ *
+ * @author Gilles Hemmerlé
+ */
+class Tcpdf extends \TCPDF {
+    //put your code here
+}
+
+?>
