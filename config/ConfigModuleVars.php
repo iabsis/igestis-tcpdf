e628719450616c01d3f42cf7fa5138030ce117c2
gillesh <gillesh@9ce8b743-0909-46e4-8230-39c73aa7ff53>
2013-06-14 07:43:08 +0000

    * Correction du module tcpdf
    git-svn-id: http://open.iabsis.com/subversion/igestis-tcpdf/trunk@1840 9ce8b743-0909-46e4-8230-39c73aa7ff53

---
+++ config/ConfigModuleVars.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This class will permitt to set all global variables of the module
+ * @Author : Gilles Hemmerlé <gilles.h@iabsis.com>
+ */
+
+namespace Igestis\Modules\Tcpdf;
+
+define("IGESTIS_TCPDF_VERSION", "0.1-1");
+define("IGESTIS_SAMBA_MODULE_NAME", "Tcpdf");
+define("IGESTIS_TCPDF_TEXTDOMAIN", IGESTIS_TCPDF_MODULE_NAME .  IGESTIS_TCPDF_VERSION);
+/**
+ * Configuration of the module
+ *
+ * @author Gilles Hemmerlé
+ */
+class ConfigModuleVars {
+
+    /**
+     * @var String Numéro de version du module
+     */
+    const version = IGESTIS_TCPDF_VERSION;
+    /**
+     *
+     * @var String Name of the module (used only on the source code) 
+     */
+    const moduleName = IGESTIS_TCPDF_MODULE_NAME;
+    
+    /**
+     *
+     * @var String Name of the menu showed to the user (blank if it is a simple service)
+     */
+    const moduleShowedName = "Tcpdf";
+    
+    /**
+     *
+     * @var String textdomain used for this module
+     */
+    const textDomain = IGESTIS_TCPDF_TEXTDOMAIN;    
+    
+    
+}
